c0f0d2d14a8ad78345bc06408830f0aff956fbb1
Nek0 <usernek0909@gmail.com>
2024-09-05 22:12:17 +0700

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 "use strict";
 
-// ligh and dark
+// Light and dark theme toggle
 
-const /** [NodeElement} */ $themeBtn = document.querySelector("data-theme-btn");
-const /** [NodeElement} */ $HTML = document.documentElement;
-let /** (Boolean | String} */ isDark= window.matchMedia("(prefers-color-scheme: dark)").matches;
+const /** [NodeElement] */ $themeBtn = document.querySelector("[data-theme-btn]");
+const /** [NodeElement] */ $HTML = document.documentElement;
+let /** (Boolean | String) */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 if (sessionStorage.getItem("theme")) {
     $HTML.dataset.theme = sessionStorage.getItem("theme");
 } else {
-    $HTML.dataset.theme = isDark? "dark": "light";
+    $HTML.dataset.theme = isDark ? "dark" : "light";
     sessionStorage.setItem("theme", $HTML.dataset.theme);
 }
 
 const changeTheme = () => {
-
-    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light"? "dark": "light";
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
     sessionStorage.setItem("theme", $HTML.dataset.theme);
+};
 
-}
+$themeBtn.addEventListener("click", changeTheme);
 
-$themeBtn.addEventListener("click", changeTheme)
+// Tab functionality
+
+const $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let lastActiveTab = document.querySelector("[data-tab-content].active");
+let lastActiveBtn = document.querySelector("[data-tab-btn].active");
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function () {
+        lastActiveTab.classList.remove("active");
+        lastActiveBtn.classList.remove("active");
+
+        const $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+
+        lastActiveTab = $tabContent;
+        lastActiveBtn = this;
+    });
+});
